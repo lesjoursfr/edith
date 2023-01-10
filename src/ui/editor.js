0c148c70a2395b87719f4e8a082aa190db8e1ded
Cécilia Haentjens <cecilia.haentjens@gmail.com>
2023-01-10 15:51:02 +0100

    doc : Add missing comment

--- src/ui/editor.js
+++ src/ui/editor.js
@@ -29,6 +29,7 @@ function EdithEditor(ctx, options) {
   // Replace &nbsp; by the string we use as a visual return
   this.content = this.content.replace(/&nbsp;/g, '<span class="edith-nbsp" contenteditable="false">¶</span>');
 
+  // Trigger the editor is loaded event
   this.ctx.trigger("edith.editor.isloaded");
 }
 
