813a548948ad6323fe21427134bcbeb3c169eda9
Cécilia Haentjens <cecilia.haentjens@gmail.com>
2023-01-18 11:17:18 +0100

    fix : Remove the wrapper in the destroy method

--- tests/editor.tests.js
+++ tests/editor.tests.js
@@ -6,14 +6,23 @@ const element = document.createElement("div");
 element.setAttribute("id", "editor");
 const edith = new Edith(element, {});
 
-test("editor.dom.setContent", (t) => {
+test("core.editor.setContent", (t) => {
   edith.setContent("<b>Bold Text</b>");
 
   t.is(edith.editor.editors.visual.innerHTML, "<b>Bold Text</b>");
 });
 
-test("editor.dom.getContent", (t) => {
+test("core.editor.getContent", (t) => {
   edith.editor.editors.visual.innerHTML = "<i></i><b><i>Italic</i> and Bold Text</b>";
 
   t.is(edith.getContent(), "<b><i>Italic</i> and Bold Text</b>");
+});
+
+test("core.editor.destroy", (t) => {
+  edith.destroy();
+
+  t.is(edith.element, undefined);
+  t.is(edith.toolbar, undefined);
+  t.is(edith.editor, undefined);
+  t.is(edith.modals, undefined);
 });
