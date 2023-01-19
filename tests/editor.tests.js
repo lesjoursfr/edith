22ec1c65a461ee3f3f244d66ce7a6910790a3977
Adrien ERAUD <e-adrien@users.noreply.github.com>
2023-01-19 09:23:48 +0100

    Update tests

--- tests/editor.tests.js
+++ tests/editor.tests.js
@@ -2,23 +2,25 @@ import test from "ava";
 import { JSDOM } from "jsdom";
 import { Edith } from "../src/index.js";
 
-/* Initialize a WYSIWYG Editor for testing */
-const dom = new JSDOM('<!DOCTYPE html><div id="editor"></div>');
-const edith = new Edith(dom.window.document.getElementById("editor"), {});
-
 test("ui.editor.setContent", (t) => {
+  const dom = new JSDOM('<!DOCTYPE html><div id="editor"></div>');
+  const edith = new Edith(dom.window.document.getElementById("editor"), {});
   edith.setContent("<b>Bold Text</b>");
 
   t.is(edith.editor.editors.visual.innerHTML, "<b>Bold Text</b>");
 });
 
 test("ui.editor.getContent", (t) => {
+  const dom = new JSDOM('<!DOCTYPE html><div id="editor"></div>');
+  const edith = new Edith(dom.window.document.getElementById("editor"), {});
   edith.editor.editors.visual.innerHTML = "<i></i><b><i>Italic</i> and Bold Text</b>";
 
   t.is(edith.getContent(), "<b><i>Italic</i> and Bold Text</b>");
 });
 
 test("ui.editor.destroy", (t) => {
+  const dom = new JSDOM('<!DOCTYPE html><div id="editor"></div>');
+  const edith = new Edith(dom.window.document.getElementById("editor"), {});
   const editor = dom.window.document.getElementById("editor");
   edith.destroy();
 
