ada806eb6a505cfe4fd5c87eaf1c1ce1872bd12d
Adrien ERAUD <e-adrien@users.noreply.github.com>
2024-09-13 13:22:23 +0200

    Sanitize pasted HTML code

--- src/core/edit.ts
+++ src/core/edit.ts
@@ -14,6 +14,7 @@ import {
   trimTag,
   unwrapNode,
 } from "@lesjoursfr/browser-tools";
+import { sanitize } from "dompurify";
 import { getSelection, moveCursorAfterNode, moveCursorInsideNode, selectNodeContents, selectNodes } from "./range.js";
 
 /**
@@ -393,7 +394,7 @@ export function cleanDomContent(root: HTMLElement, style: { [keyof: string]: boo
 export function cleanPastedHtml(html: string, style: { [keyof: string]: boolean }): HTMLElement {
   // Create a new div with the HTML content
   const result = document.createElement("div");
-  result.innerHTML = html;
+  result.innerHTML = sanitize(html, { USE_PROFILES: { html: true } });
 
   // Clean the HTML content
   cleanDomContent(result, style);
