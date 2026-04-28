c66b513c4504ba72b83aacdee4c0a1564b46d8b9
Adrien ERAUD <e-adrien@users.noreply.github.com>
2026-04-28 13:48:22 +0100

    Update repo

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,12 @@
 import eslint from "@eslint/js";
 import prettierConfig from "eslint-config-prettier";
+import { defineConfig } from "eslint/config";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 
-export default tseslint.config(
+export default defineConfig(
   eslint.configs.recommended,
-  ...tseslint.configs.recommended,
+  tseslint.configs.recommended,
   {
     languageOptions: {
       parserOptions: {
@@ -23,8 +24,8 @@ export default tseslint.config(
   {
     ignores: [
       "package.json",
-      "eslint.config.mjs",
-      "prettier.config.mjs",
+      "eslint.config.js",
+      "prettier.config.js",
       "node_modules/*",
       ".yarn/*",
       "server/builds/*",
